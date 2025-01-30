9c2596bdb2c4196dfff9f3cee65b0cbc68253905
Marcela Ananias <marcelaananias154@gmail.com>
2025-01-30 16:40:28 -0300

    Adjust product creation and editing by adding images

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,8 +6,10 @@ use App\Entity\Product;
 use App\Entity\SubCategory;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProductType extends AbstractType
 {
@@ -17,6 +19,23 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
+            ->add('image', FileType::class, [
+                'label' => 'Product image',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        "maxSize" =>"1024k",
+                        "mimeTypes" =>[
+                            'image/png',
+                            'image/jpg',
+                            'image/jpeg'
+                        ],
+                        'maxSizeMessage' => 'The uploaded file is too large. The maximum allowed size is 1MB.',
+                        'mimeTypesMessage'=> "The uploaded file must be an image in PNG, JPG or JPEG format."
+                    ])
+                ]
+            ])
             ->add('subCategories', EntityType::class, [
                 'class' => SubCategory::class,
                 'choice_label' => 'name',
